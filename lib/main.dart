72cf1c1ad1c624c8204ca382189032ad6c207413
Your Name <you@example.com>
2026-05-25 21:25:07 +0300

    update flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 247, 237, 236),
+        body: SafeArea(
+            child: Column(
+          children: [
+            CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/PersonalImage.png")),
+            Text(
+              "Dina Taha Nada",
+              style: TextStyle(
+                fontFamily: 'LucidaBrightRegular',
+                fontSize: 30,
+                color: const Color.fromARGB(255, 150, 6, 6),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'flutter developer',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home")
-      ),
-      drawer: Drawer(),
-      body: Center(
-        child: Text("dinatha",
-        style: TextStyle(fontSize: 30),
-       ),
+              ),
+
+            ),
+            Container(
+              color: Colors.blue,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Icon(Icons.phone, color: Colors.white,),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Text(
+                    '+201033333333',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    ),
+                ],
+                    
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Icon(Icons.email, color: Colors.white,),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Text(
+                    'dinataha@email.com',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    ),
+                ],
+                    
+              ),
+            ),
+             
+          
+          ],
+        )),
       ),
     );
   }
